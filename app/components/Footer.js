077640a815b9a0d777152921d4e2570402aa2aa0
Zayn Yusuf <yusue786@gmail.com>
2025-01-13 20:18:32 +0600

    fix space

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -5,30 +5,36 @@ function Footer() {
     <div className="mt-[24px] lg:mt-[60px] bg-black">
       <div className="container pt-[44px] lg:pt-[18px] pb-[40px] lg:pb-[30px]">
         <div className="flex">
-          <h4 className="hidden lg:block text-[22px] mr-[90px] font-medium uppercase">
+          <h4 className="hidden lg:block text-[22px] mr-[90px] font-medium uppercase text-[#9B9B9B]">
             EROSADS
           </h4>
 
           <div className="flex justify-evenly flex-wrap gap-x-[55px] gap-y-5 lg:gap-y-[27px] text-xs lg:text-sm text-white/80">
             <div className="">
-              <p className="cursor-pointer hover:text-custom-amber">Contact</p>
-              <p className="cursor-pointer hover:text-custom-amber">
+              <p className="cursor-pointer hover:text-custom-amber text-[#9B9B9B]">
+                Contact
+              </p>
+              <p className="cursor-pointer hover:text-custom-amber text-[#9B9B9B]">
                 Terms & Conditions
               </p>
             </div>
             <div className="">
-              <p className="cursor-pointer hover:text-custom-amber">F.A.Q.</p>
-              <p className="cursor-pointer hover:text-custom-amber">Privacy</p>
+              <p className="cursor-pointer hover:text-custom-amber text-[#9B9B9B]">
+                F.A.Q.
+              </p>
+              <p className="cursor-pointer hover:text-custom-amber text-[#9B9B9B]">
+                Privacy
+              </p>
             </div>
             <div className="">
-              <p className="cursor-pointer hover:text-custom-amber">
+              <p className="cursor-pointer hover:text-custom-amber text-[#9B9B9B]">
                 Advertiser Agreement
               </p>
-              <p className="cursor-pointer hover:text-custom-amber">
+              <p className="cursor-pointer hover:text-custom-amber text-[#9B9B9B]">
                 About ErosAds
               </p>
             </div>
-            <p className="cursor-pointer hover:text-custom-amber">
+            <p className="cursor-pointer hover:text-custom-amber text-[#9B9B9B]">
               DMCA Complaints
             </p>
           </div>
