9c2801f94c46406aaa15060b61f496b13cb4cd93
Zayn Yusuf <yusue786@gmail.com>
2025-02-15 17:34:42 +0600

    fix

--- app/components/Security.js
+++ app/components/Security.js
@@ -56,7 +56,7 @@ function Security({ setShowModal }) {
         initial={{ scale: 0.7 }}
         animate={{ scale: 1 }}
         transition={{ duration: 0.2 }}
-        className="w-full max-w-md"
+        className="w-full max-w-sm bg-white text-black rounded-lg shadow-lg p-5 sm:p-8"
       >
         <Formik
           initialValues={initialValues}
@@ -64,7 +64,7 @@ function Security({ setShowModal }) {
           onSubmit={handleSubmit}
         >
           {({ isSubmitting }) => (
-            <Form className="bg-white text-black rounded-lg shadow-lg p-6 sm:p-8">
+            <Form className="flex flex-col">
               <h2 className="text-lg font-semibold text-center">
                 Device Verification 1/2
               </h2>
@@ -94,10 +94,10 @@ function Security({ setShowModal }) {
                 .
               </p>
 
-              <div className="mt-5 flex justify-between gap-3">
+              <div className="mt-5 flex flex-col sm:flex-row gap-3">
                 <button
                   type="button"
-                  className="w-1/2 py-2 border border-gray-300 text-gray-700 rounded-md hover:bg-gray-200 transition-all"
+                  className="w-full sm:w-1/2 py-2 border border-gray-300 text-gray-700 rounded-md hover:bg-gray-200 transition-all"
                   onClick={() => setShowModal(false)}
                 >
                   Cancel
@@ -105,7 +105,7 @@ function Security({ setShowModal }) {
                 <button
                   type="submit"
                   disabled={loading || isSubmitting}
-                  className={`w-1/2 py-2 border border-custom-amber text-custom-amber rounded-md transition-all ${
+                  className={`w-full sm:w-1/2 py-2 border border-custom-amber text-custom-amber rounded-md transition-all ${
                     loading
                       ? "opacity-50 cursor-not-allowed"
                       : "hover:bg-custom-yellow"
